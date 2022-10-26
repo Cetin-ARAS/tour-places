481054a071a6d2d9a64b3ef8175bcc0649a65591
ÇETİN <cetinnnarasss@gmail.com>
2022-10-27 01:10:47 +0300

    cont.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./Main.scss";
+import { data } from "../../helpers/data";
+import Card from './Card';
 
 const Main = () => {
+    console.log(data);
   return (
-    <div className='card-container'></div>
+    <div className='card-container'>
+        {data.map((item)=> ( 
+        <Card {...item} key={index} />
+         ))}
+    </div>
   )
-}
+};
 
 export default Main
